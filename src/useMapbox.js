1831e0279753452a0799e7b79af7e9aa0e09219f
Juliapirat <109736239+Juliapirat@users.noreply.github.com>
2022-10-26 21:40:07 +0300

    Update useMapbox.js

--- src/useMapbox.js
+++ src/useMapbox.js
@@ -1,34 +1,15 @@
-import React, { useState } from "react";
-import { render } from "react-dom";
-import useMapbox from "./useMapbox.js";
-import "./tailwind.output.css";
+import { useLayoutEffect } from "react";
+import mapboxgl from "mapbox-gl";
 
-function App() {
-  const [, setRandom] = useState(Math.random());
-
-  let container = "map";
-  let center = [21, 21];
-  let zoom;
-  let [long = 0, lat = 0] = center;
-
-  useMapbox(container, [long, lat], zoom);
-  return (
-    <div className="min-h-screen bg-gray-100 ml-100">
-      <div className="flex  flex-col items-center justify-center">
-        <button
-          className="bg-indigo-500 mt-10 mb-10 p-4 w-40 text-white rounded hover:bg-indigo-600 "
-          id="rerender"
-          onClick={() => setRandom(Math.random())}
-        >
-          Ререндер!
-        </button>
-        <div
-          className="w-5/6 border-solid border-2 border-indigo-600 rounded-lg"
-          id="map"
-        ></div>
-      </div>
-    </div>
-  );
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoib3RyaWNhbGEiLCJhIjoiY2tqMjFjcTNzMnp2djJ1bW05NW9zd2h6NCJ9.0w535oE4G9JjDpDxC_vVuw";
+export default function useMapbox(container = "map", [long, lat], zoom = 2) {
+  useLayoutEffect(() => {
+    new mapboxgl.Map({
+      container: container,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [long, lat],
+      zoom: zoom
+    });
+  }, [zoom, container, long, lat]);
 }
-
-render(<App />, document.querySelector("#root"));
