ac06a56ea605c58ac406bb45ceeca8ed9bab2dd7
Juliapirat <109736239+Juliapirat@users.noreply.github.com>
2022-10-26 12:55:19 +0300

    Update useMapbox.js

--- src/useMapbox.js
+++ src/useMapbox.js
@@ -1,16 +1,16 @@
 import { useLayoutEffect } from "react";
 import mapboxgl from "mapbox-gl";
 
+const container = "map";
+const center = [0, 0];
+const zoom = 2;
+
 mapboxgl.accessToken =
   "pk.eyJ1Ijoib3RyaWNhbGEiLCJhIjoiY2tqMjFjcTNzMnp2djJ1bW05NW9zd2h6NCJ9.0w535oE4G9JjDpDxC_vVuw";
 
-export default function useMapbox(
-  container = "map",
-  center = [0, 0],
-  zoom = 2
-) {
+export default function useMapbox() {
   useLayoutEffect(() => {
-    const map = new mapboxgl.Map({
+    new mapboxgl.Map({
       container: container,
       style: "mapbox://styles/mapbox/streets-v11",
       center: center,
